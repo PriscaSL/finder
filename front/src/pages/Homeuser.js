36d6a11f17b3cf523f7478f4b52493f436aa180a
prisca <prisca@localhost.localdomain>
2024-06-21 17:27:35 +0300

    brouillons

--- front/src/pages/Homeuser.js
+++ front/src/pages/Homeuser.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import '../styles/Homeuser.css' 
+import profile from '../assets/rakoto.jpg'
+import BoltIcon from '@mui/icons-material/Bolt';
+
 
 function Homeuser() {
   return (
     <div className='Homeuser'>
-      <Sidebar/>
-      <div className='DashBoard'>
+       <div className='Sidebar'>
 
-      </div>
+       </div>
+       <div className='Dashboard'>
+            <div className='welcomtext'>
+              <img src={profile}/>
+              <p id='Beginner'>Beginner</p>
+              <p id='px'><BoltIcon/> 0 px</p>
+            </div>
+       </div>
+       
     </div>
   )
 }
