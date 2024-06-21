d61af8e9dd180527fd5e0f3a258f933e96e07d92
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-06-21 21:23:14 +0300

    change langing page

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import '../styles/Nav.css';
 import { Link } from 'react-router-dom'; 
+import Logo from '../assets/LOGO GLISH NOIR.png';
 
 function Navbar() {
   return (
     <nav>
       <div className="leftSide">
-        {/*<img src='' alt="Logo" />*/}
+        <img src={Logo} alt="Logo" className='logo' />
       </div>
       <div className='rightSide' id='navbar'>
-         
         <Link to="/why-us">Why us</Link>
-        <Link to="/homeuser">Services-Homeuserpermanent-</Link>
-        
-        <button id='btnnobg' onClick={(e) => window.location.href ="/login"}>Sign in</button>
-        <button id='btnwbg' onClick={(e) => window.location.href ="/login"}>Start for free</button>
+        <Link to="/homeuser">Services</Link>
+        <Link to="/homeuser">Contact</Link>
+        <button onClick={(e) => window.location.href ="/login"} className='signin-link'>Sign in</button>
+        <button onClick={(e) => window.location.href ="/login"} className='start-for-free-link'>Start for free</button>
       </div>
     </nav>
   );
--- front/src/pages/Landing.js
+++ front/src/pages/Landing.js
@@ -1,49 +1,34 @@
 import React from 'react'
 import '../styles/Landing.css'
 import imgland from '../assets/hero-img.png'
-import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+;import ModuleList from '../helpers/ModuleList';
 
 function Landing() {
 	return (
 		<>
-			<Navbar/>
+			<Navbar />
 			<div className="landing" >
-				<div className="partbody">
-
-					<div className='textland'>
-						<h1>The best way to <br /> learn</h1>
-						<button id='btnpurple'><Link to='/login'>Get started for free</Link></button>
-						<p id='textland'>Join 300K+ members working in companies like</p>
-						<div className='imagepartenaire'>
-							<img src={imgland} />
-							<label>hhgg</label>
-							<img src={imgland} />
-							<label>hhgg</label>
-							<img src={imgland} />
-							<label>hhgg</label>
-							<img src={imgland} />
-							<label>hhgg</label>
-						</div>
-					</div>
-
-					<div className='imgland'>
-						<img src={imgland} />
-					</div>
-
+				<div className="leftSide-hero">
+					<h1>The best way <span>To learn</span> English</h1>
+					<p>Unlock your potential with Glish, the ultimate English learning platform that adapts to your needs.</p>
+					<button onClick={() => window.location.href = "/login"} className="start-for-free-btn">Start for free</button>
 				</div>
-				<div className="partfoot">
-					<img src={imgland} />
-					<label>hhgg</label>
-					<img src={imgland} />
-					<label>hhgg</label>
-					<img src={imgland} />
-					<label>hhgg</label>
-					<img src={imgland} />
-					<label>hhgg</label>
-					<img src={imgland} />
-					<label>hhgg</label>
+				<div className="rightSide-hero">
+					<img src={imgland} alt="Landing" className='imgland' />
 				</div>
+			</div>
+			<div className='landing-module'>
+				{ModuleList.map((moduleItem, key) => {
+					return (
+							<div className='module-item'>
+								<div className='icon' style={{backgroundColor: moduleItem.bg}}>
+									{moduleItem.image}
+								</div>
+								<h4>{moduleItem.tilte}</h4>
+							</div>
+					)
+				})}
 			</div>
 		</>
 	)
