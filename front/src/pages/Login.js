5e442431c4fa4d61923aee9c2ef0476c6b0bc189
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-06-21 18:54:05 +0300

    add login logic

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,20 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Login.css'
 import IconGlish from '../assets/ICONE GLISH.png';
 import Google from '../assets/google.png';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const LOGIN_URL = 'https://zahageek-back.onrender.com/api/auth/login';
+
+    const submitForm = (e) => {
+        setIsLoading(true);
+        e.preventDefault();
+        setError('');
+        const data = {email: email, password: password};
+        const request = loginPost(data);
+        request.then(response => response.json())
+        .then(data => {
+            if(data.status == false) {
+                setError(data.message);
+                setIsLoading(false);
+            } else {
+                localStorage.setItem('token', data.user.token);
+                setIsLoading(false);
+                window.location.href = '/homeuser';
+            }
+        });
+    }
+
+    const loginPost = async (data) => {
+        return await fetch(LOGIN_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    }
+
     return (
         <div className='container'>
             <div className='logo'><img src={IconGlish} alt='logo' /></div>
             <div className='login-form'>
                 <h3 className='login-intro'>Welcome back to Glish</h3>
                 <p className='login-p'>Don't have an account? <Link to='/register' className='form-link'>Sign up</Link></p>
-                <form>
-                    <input type='email' placeholder='Email' />
-                    <input type='password' placeholder='Password' />
-                    <button className='btn-signin'>Sign in</button>
+                <form onSubmit={(e) => submitForm(e)}>
+                    {error && <p className='error'>{error}</p>}
+                    <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                    <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                    <button 
+                        className={!isLoading ? 'btn-signin' : 'btn-signin btn-signin-disabled'} 
+                        type='submit'
+                        disabled={isLoading ? "disabled" : ""}
+                    >
+                        {isLoading && <CircularProgress style={{width: "20px", height: "20px"}}/>}
+                        Sign in
+                    </button>
                     <Link to='/' className='form-link'>Forgot your password?</Link>
                 </form>
                 <div className='division'>
