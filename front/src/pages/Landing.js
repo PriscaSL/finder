4315791070368ff09ffe773e32cd6f7f380480dd
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-06-21 22:27:19 +0300

    add presentation

--- front/src/pages/Landing.js
+++ front/src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/Landing.css'
-import imgland from '../assets/hero-img.png'
+import imgland from '../assets/hero.png'
 import Navbar from '../components/Navbar'
 ;import ModuleList from '../helpers/ModuleList';
 import Footer from '../components/Footer';
@@ -31,6 +31,21 @@ function Landing() {
 					)
 				})}
 			</div>
+			<section className='presentation'>
+				<h1>GLISH help users master the English language!</h1>
+				<button>Get Started For Free</button>
+			</section>
+			<div class="partenaire">
+            <div class="division"></div>
+            <div class="partenaire-list">
+                <div><img src={} alt="partenaire"/></div>
+                <div><img src={} alt="partenaire"/></div>
+                <div><img src={} alt="partenaire"/></div>
+                <div><img src={} alt="partenaire"/></div>
+                <div><img src={} alt="partenaire"/></div>
+            </div>
+            <div class="division"></div>
+        </div>
 			<Footer	/>
 		</>
 	)
